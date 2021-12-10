93257006c79af26bbd41a55d66c0dbcbe6441d6d
Magesh Kuppan <tkmagesh77@gmail.com>
2021-12-10 12:16:14 +0530

    filter the bugs by the selected project

--- bug-tracker-app/src/projects/index.js
+++ bug-tracker-app/src/projects/index.js
@@ -1,5 +1,7 @@
 import * as projectActionCreators from './actions';
 import { useDispatch, useSelector } from 'react-redux';
+import './index.css';
+
 const Projects = () => {
     const projects = useSelector(storeState => storeState.projectsState);
 
@@ -7,6 +9,7 @@ const Projects = () => {
         <div className="projects">
         <h3>Projects</h3>
         <ol>
+            {/* <li className="selected">[Selected Project]</li> */}
             {projects.map(project => (
             <li key={project.id}>
                 <div>{project.name}</div>
