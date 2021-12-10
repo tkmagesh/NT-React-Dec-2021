2ffa18c753a54f1fde922a57ba177f3de74393ff
Magesh Kuppan <tkmagesh77@gmail.com>
2021-12-10 15:43:24 +0530

    async demos

---
+++ 07-async/async-demos.js
@@ -0,0 +1,66 @@
+(function(){
+    function addSync(x,y){
+        console.log(`   [@service] processing ${x} and ${y}`)
+        var result = x + y;
+        console.log(`   [@service] returning result`)
+        return result;
+    }
+
+    function addSyncClient(x,y){
+        console.log(`[@client] triggering the service`)
+        var result = addSync(x,y);
+        console.log(`[@client] result = ${result}`)
+    }
+
+    window['addSyncClient'] = addSyncClient;
+
+    function addAsync(x,y, callback){
+        console.log(`   [@service] processing ${x} and ${y}`)
+        setTimeout(() => {
+            var result = x + y;
+            console.log(`   [@service] returning result`)
+            callback(result);
+        }, 4000);
+    }
+
+    function addAsyncClient(x,y){
+        console.log(`[@client] triggering the service`)
+        addAsync(x,y, function(result){
+            console.log(`[@client] result = ${result}`)
+        });
+    }
+
+    window['addAsyncClient'] = addAsyncClient;
+
+    function addAsyncPromise(x,y){
+        console.log(`   [@service] processing ${x} and ${y}`)
+        const p = new Promise(function(resolveFn, rejectFn){
+            setTimeout(() => {
+                var result = x + y;
+                console.log(`   [@service] returning result`)
+                resolveFn(result)
+            }, 4000);
+        });        
+        return p;
+    }
+
+    /* 
+    function addAsyncPromiseClient(x,y){
+        console.log(`[@client] triggering the service`);
+        var p = addAsyncPromise(100,200)
+        p.then(function(result){
+            console.log(`[@client] result =${result}`);
+        });
+    } 
+    */
+
+    async function addAsyncPromiseClient(x,y){
+        console.log(`[@client] triggering the service`);
+        var result = await addAsyncPromise(100,200)
+        console.log(`[@client] result =${result}`);
+        return result * 2
+    }
+
+    window['addAsyncPromiseClient'] = addAsyncPromiseClient
+})();
+
